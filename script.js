28f5dc7f679f493d0150cedbda35200dab1f71ba
poojithalakshmigari <lakshmigaripoojitha678@gmail.com>
2026-03-16 15:59:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function updateData(){
+
+let users = Math.floor(Math.random()*1000);
+let sales = Math.floor(Math.random()*500);
+
+document.getElementById("users").innerText = users;
+document.getElementById("sales").innerText = sales;
+
+}
